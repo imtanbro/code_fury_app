1a72a0103a01c324daf719cc563fa55a7dce345a
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-11-07 23:56:39 +0530

    Job List and Status

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:codefuryapp/Employee/Screens/HomeScreen/home.dart';
 import 'package:codefuryapp/Employee/Screens/Registration.dart';
 import 'package:codefuryapp/Service/Login/login.dart';
 import 'package:flutter/material.dart';
@@ -13,84 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: SelectPart(),
-    );
-  }
-}
-
-// NavigationScreen()
-
-class SelectPart extends StatefulWidget {
-  @override
-  _SelectPartState createState() => _SelectPartState();
-}
-
-class _SelectPartState extends State<SelectPart> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            children: [
-              Container(
-                height: 200,
-                width: (MediaQuery.of(context).size.width / 2) - 25,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        colors: [Colors.blue, Colors.black.withOpacity(0.5)],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter)),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 68, horizontal: 10),
-                      child: Text("Looking for Job",
-                          style: TextStyle(
-                            fontSize: 25,
-                          )),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => NavigationScreen()));
-                },
-                child: Container(
-                  height: 200,
-                  width: (MediaQuery.of(context).size.width / 2) - 25,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: [Colors.blue, Colors.black.withOpacity(0.5)],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter)),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 68, horizontal: 10),
-                        child: Text("Creating Opportunities",
-                            style: TextStyle(
-                              fontSize: 25,
-                            )),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
